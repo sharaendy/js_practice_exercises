fac1a837049bc8d37d28cbf7e0f5064a798efdce
andrey.sh <aash7133@gmail.com>
2022-05-02 21:33:34 +0700

    добавил Фасад

---
+++ materials/01 введение в программирование/испытания/13. Фасад/fasad.js
@@ -0,0 +1,7 @@
+import cube from './numbers1';
+import multi, { pow2 as pow, sum2 as sum, sub2 as sub } from './numbers2';
+import sqrt from './numbers3';
+
+export {
+  cube as default, multi, pow, sum, sub, sqrt,
+};
