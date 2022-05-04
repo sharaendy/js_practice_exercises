e4cb54fd6645cd7c563f6a64dfc6bf3e6e370222
andrey.sh <aash7133@gmail.com>
2022-05-04 15:51:18 +0700

    3.объекты: испытание 6

--- materials/03 объекты/испытания/6. Скрэббл/scrabble.js
+++ materials/03 объекты/испытания/6. Скрэббл/scrabble.js
@@ -1,3 +1,32 @@
+//! лучшее
+// Эту функцию можно заменить функцией countBy
+// из библиотеки lodash https://lodash.com/docs/#countBy
+const countByChars = (str) => {
+  const chars = {};
+
+  for (const char of str) {
+    const count = _.get(chars, char, 0);
+    chars[char] = count + 1;
+  }
+
+  return chars;
+};
+
+export default (str, word) => {
+  const countsChars = countByChars(str);
+
+  for (const char of word.toLowerCase()) {
+    const count = _.get(countsChars, char, 0);
+    if (count === 0) {
+      return false;
+    }
+    countsChars[char] -= 1;
+  }
+
+  return true;
+};
+
+//! решение 1
 function scrabble(chars, word) {
   if (chars.length < word.length) return false;
 
@@ -5,9 +34,9 @@ function scrabble(chars, word) {
     const result = {};
     for (const name of str) {
       if (Object.hasOwn(result, name)) {
-        result[name] += 1;
+        result[name.toLowerCase()] += 1;
       } else {
-        result[name] = 1;
+        result[name.toLowerCase()] = 1;
       }
     }
     return result;
