a4b31d12a9ea80932e82d131e3fee7c7199b4ff8
andrey.sh <aash7133@gmail.com>
2022-06-15 20:57:23 +0700

    addes

--- src/script.js
+++ src/script.js
@@ -1,38 +1,11 @@
 import fs from 'fs';
+// const { promises: fsp } = fs;
 
-function move(file, dir, cb) {
-  fs.readFile(file, 'utf-8', (err1, done) => {
-    if (err1) {
-      cb(err1);
-      return;
-    }
-    fs.writeFile(dir, done, (err2) => {
-      if (err2) {
-        cb(err2);
-        return;
-      }
-      fs.unlink(file, (err3) => {
-        if (err3) {
-          cb(err3);
-          return;
-        }
-        cb(null);
-      });
-    });
-  });
+function reverse(filepath) {
+  fs.promises.readFile(filepath, 'utf-8')
+  .then((content) => console.log('данные получены'))
+  .then(())
 }
 
-const file1 = '/home/aash7133/projects/js_practice_exercises/file1.txt';
-const dir = '/home/aash7133/projects/js_practice_exercises/dir/file3.txt';
-
-move(file1, dir, (error) => {
-  if (error) {
-    console.log('error');
-    return;
-  }
-  console.log('yes!');
-});
-
-// import fs from 'fs';
-// const abs = fs.readFileSync('/file1.txt');
-// console.log(abs);
+const file = '/home/aash7133/projects/js_practice_exercises/file1.txt';
+reverse(file);
