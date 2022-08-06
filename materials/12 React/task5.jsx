eba6530abe9e420831050dd4329d082486761d04
andrey.sh <aash7133@gmail.com>
2022-08-06 12:19:42 +0700

    task5 react add

--- materials/12 React/task5.jsx
+++ materials/12 React/task5.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 
+// В этом задании необходимо создать компонент, выводящий список определений.
+
+// dl – тег, который используется при создании списков определений. Совместно с этим тегом используется тег dt (содержит название определения) и dd (описание определения):
+
+// <dl>
+//   <dt>Coffee</dt>
+//   <dd>Black hot drink</dd>
+//   <dt>Milk</dt>
+//   <dd>White cold drink</dd>
+// </dl>
+// src/DefinitionsList.jsx
+// Реализуйте и экспортируйте по умолчанию компонент DefinitionsList, который принимает свойство data следующей структуры:
+
 // const definitions = [
 //   { dt: 'one', dd: 'two', id: 1 },
 //   { dt: 'another term', dd: 'another description', id: 2 },
 // ];
+
+// <DefinitionsList data={definitions} />;
+// Результатом должен быть следующий вывод:
+
+// <dl>
+//   <dt>one</dt>
+//   <dd>two</dd>
+//   <dt>another term</dt>
+//   <dd>another description</dd>
+// </dl>
+// Если data — это пустой массив, то ничего не должно рендериться
+
 //! решение 1
 export default class DefinitionsList extends React.Component {
   render() {
