f5892a52352d75f71a5830fc6ee20100f3d8845c
andrey.sh <aash7133@gmail.com>
2022-09-18 12:21:14 +0700

    materials added

--- materials/0 алгоритмы/hexlet.js
+++ materials/0 алгоритмы/hexlet.js
@@ -1,26 +1,12 @@
-/* eslint-disable */
-const input = {
-  ' ': [5],
-  d: [10],
-  e: [1],
-  H: [0],
-  l: [2, 3, 9],
-  o: [4, 7],
-  r: [8],
-  w: [6],
-};
-
-function buildString(obj) {
-  const result = [];
-
-  for (const [key, value] of Object.entries(obj)) {
-    if (value.length > 1) {
-      value.forEach(item => result[item] = key);
-    } else if (value.length === 1) {
-      result[value[0]] = key;
-    }
+/* eslint-enable */
+const obj = {
+  str: 'hello',
+  a() {
+    return this.str;
   }
-  return result.join('');
 }
 
-console.log(buildString(input));
+let temp = obj.a;
+const newTemp = temp.bind(obj);
+console.log(newTemp())
+// console.log(temp.call(obj));
--- src/index.js
+++ src/index.js
@@ -1,26 +1,2 @@
 /* eslint-disable */
-const input = {
-  ' ': [5],
-  d: [10],
-  e: [1],
-  H: [0],
-  l: [2, 3, 9],
-  o: [4, 7],
-  r: [8],
-  w: [6],
-};
 
-function buildString(obj) {
-  const result = [];
-
-  for (const [key, value] of Object.entries(obj)) {
-    if (value.length > 1) {
-      value.forEach(item => result[item] = key);
-    } else if (value.length === 1) {
-      result[value[0]] = key;
-    }
-  }
-  return result.join('');
-}
-
-console.log(buildString(input));
