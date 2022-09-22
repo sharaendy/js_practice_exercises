d74dc784aa3e0cf0da8c62d7407a3318ef4176bd
andrey.sh <aash7133@gmail.com>
2022-09-22 23:06:16 +0700

    new practice

--- materials/практика/training/allAnagrams.js
+++ materials/практика/training/allAnagrams.js
@@ -1,13 +1,17 @@
+// ### Анаграммы
+
+// Напишите функцию, которая проверяет, являются ли все элементы в массиве анаграммами друг друга.
+
 function allAnagrams(array) {
   const reference = array[0].split('').sort().join();
+  let result = true;
 
   array.forEach((item) => {
     if (item.split('').sort().join() !== reference) {
-      return false;
+      result = false;
     }
-    return null;
   });
-  return true;
+  return result;
 }
 
 console.log(allAnagrams(['abcd', 'bdac', 'cabd'])); // true
